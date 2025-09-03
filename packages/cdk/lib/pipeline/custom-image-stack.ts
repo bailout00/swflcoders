3de344b3b876bb3520567386c83e06c1a388001d
bailout00 <bailout00@gmail.com>
2025-09-03 16:06:13 -0400

    Pipeline2

--- packages/cdk/lib/pipeline/custom-image-stack.ts
+++ packages/cdk/lib/pipeline/custom-image-stack.ts
@@ -2,7 +2,7 @@ import { Stack, type StackProps } from 'aws-cdk-lib'
 import { DockerImageAsset, Platform } from 'aws-cdk-lib/aws-ecr-assets'
 import type { Construct } from 'constructs'
 import type { PipelineConfig } from '../config'
-import * as path from 'path'
+import { join } from 'path'
 
 export interface CustomImageStackProps extends StackProps {
     pipelineConfig: PipelineConfig
@@ -16,7 +16,7 @@ export class CustomImageStack extends Stack {
 
         // Build the custom image locally (during synth/deploy) and push to ECR via CDK Assets
         // Use process.cwd() which points to packages/cdk when running with ts-node
-        const buildImagePath = path.join(process.cwd(), 'build-image')
+        const buildImagePath = join(process.cwd(), 'build-image')
 
         const asset = new DockerImageAsset(this, 'PipelineBuildImage', {
             directory: buildImagePath,
